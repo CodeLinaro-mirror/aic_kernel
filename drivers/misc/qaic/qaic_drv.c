f8f7b0fafb85af721c6c33bbf0cb21af404c7001
Jeffrey Hugo <jhugo@codeaurora.org>
2019-12-10 14:48:09 -0700

    qaic: Add module version information
    Add module version information to allow userspace to determine compatibility
    with the driver.  The version consists of a major/minor/patch tuple.
    Major represents interface changes.
    Minor represents feature changes.
    Patch represents bug fixes and internal changes which are not visible to
    userspace.
    
    Change-Id: I014df4b0cca27283ce9967b8c77cd7a5f2e1ff13
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

--- drivers/misc/qaic/qaic_drv.c
+++ drivers/misc/qaic/qaic_drv.c
@@ -484,3 +484,4 @@ module_exit(qaic_exit);
 MODULE_AUTHOR("Qualcomm Cloud AI 100 Accelerator Kernel Driver Team");
 MODULE_DESCRIPTION("Qualcomm Cloud 100 AI Accelerators Driver");
 MODULE_LICENSE("GPL v2");
+MODULE_VERSION("1.0.0"); /* MAJOR.MINOR.PATCH */
