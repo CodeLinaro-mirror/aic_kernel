27c665f5fb0c49b6dbf3e1062c85aca8fc2f3275
Jeffrey Hugo <jhugo@codeaurora.org>
2020-01-20 10:15:10 -0700

    qaic: Add module parameter to tune default of wait exec ioctl timeout
    The wait_for_exec ioctl has a default timeout of 5 seconds.  Some platform
    stress testing is expected to run for multiple hours, which far exceeds
    the timeout of 5 seconds.
    
    Provide a module parameter to allow the timeout value to be tuned as
    needed for such testing.  The value is specified in milliseconds, and has
    an effective upper bound of 2^31-1, or roughly 24 days.  Values greater
    than this are intrepreted as "infinite".
    
    Change-Id: Ief8166778bc3ea4654860561be769be56e38fd51
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

--- drivers/misc/qaic/qaic_drv.c
+++ drivers/misc/qaic/qaic_drv.c
@@ -528,4 +528,4 @@ module_exit(qaic_exit);
 MODULE_AUTHOR("Qualcomm Cloud AI 100 Accelerator Kernel Driver Team");
 MODULE_DESCRIPTION("Qualcomm Cloud 100 AI Accelerators Driver");
 MODULE_LICENSE("GPL v2");
-MODULE_VERSION("1.2.3"); /* MAJOR.MINOR.PATCH */
+MODULE_VERSION("1.3.0"); /* MAJOR.MINOR.PATCH */
