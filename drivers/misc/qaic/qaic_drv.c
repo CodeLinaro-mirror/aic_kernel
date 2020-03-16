681ff2bdb76a2f0a916b49a6117844d21b212626
Jeffrey Hugo <jhugo@codeaurora.org>
2020-03-16 10:18:09 -0600

    qaic: Put telemetry behind a kconfig option
    Put the telemetry functionality behind a kconfig option.  This supports
    users which desire to disable the functionality.
    
    Change-Id: Ifaf033dc0324586298786d7f002d365dad7f3624
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

--- drivers/misc/qaic/qaic_drv.c
+++ drivers/misc/qaic/qaic_drv.c
@@ -744,4 +744,4 @@ module_exit(qaic_exit);
 MODULE_AUTHOR("Qualcomm Cloud AI 100 Accelerator Kernel Driver Team");
 MODULE_DESCRIPTION("Qualcomm Cloud 100 AI Accelerators Driver");
 MODULE_LICENSE("GPL v2");
-MODULE_VERSION("1.7.10"); /* MAJOR.MINOR.PATCH */
+MODULE_VERSION("1.7.11"); /* MAJOR.MINOR.PATCH */
