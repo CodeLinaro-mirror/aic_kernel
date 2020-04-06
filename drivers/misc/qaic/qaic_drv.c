be9e49a39d587ca2a73de1e0b8041768b56473ef
Jeffrey Hugo <jhugo@codeaurora.org>
2020-04-06 13:24:42 -0600

    qaic: Implement mhi link status callback
    When the MHI core thinks that it detects that the link has gone down, it
    will call back to the controller for a confirmation.  This is done via the
    link_status() callback.  Our dummy implementation always returns
    "all good", which causes the MHI core to proceed, which may result in bad
    data.  Instead, implement a check that will return if the link is up or
    down.
    
    Change-Id: Iaf2cd1c7e799090eb29eff3bc56253249c145fce
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

--- drivers/misc/qaic/qaic_drv.c
+++ drivers/misc/qaic/qaic_drv.c
@@ -757,4 +757,4 @@ module_exit(qaic_exit);
 MODULE_AUTHOR("Qualcomm Cloud AI 100 Accelerator Kernel Driver Team");
 MODULE_DESCRIPTION("Qualcomm Cloud 100 AI Accelerators Driver");
 MODULE_LICENSE("GPL v2");
-MODULE_VERSION("2.0.6"); /* MAJOR.MINOR.PATCH */
+MODULE_VERSION("2.0.7"); /* MAJOR.MINOR.PATCH */
