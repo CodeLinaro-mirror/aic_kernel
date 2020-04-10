7c8923eaad83b7fe4fa6ff0effb60f9feae3a234
Jeffrey Hugo <jhugo@codeaurora.org>
2020-04-10 15:43:06 -0600

    qaic: Request a completion only for the last request
    We expect the DBC completion to occur when the request is complete.
    Thus, we must only signal a completion on the last segment, otherwise we
    will receive "early" completions and incorrectly assume the request is
    complete.
    
    Change-Id: I5ccc9da61f2635ee8ce76e5fb3ca37fe78be9ea0
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

--- drivers/misc/qaic/qaic_data.c
+++ drivers/misc/qaic/qaic_data.c
@@ -449,7 +449,7 @@ static bool invalid_sem(struct qaic_sem *sem)
 static int encode_execute(struct qaic_device *qdev, struct mem_handle *mem,
 			  struct qaic_execute *exec, u16 req_id)
 {
-	u8 cmd = BULK_XFER | GEN_COMPLETION;
+	u8 cmd = BULK_XFER;
 	u64 db_addr = cpu_to_le64(exec->db_addr);
 	u8 db_len;
 	u32 db_data = cpu_to_le32(exec->db_data);
@@ -550,6 +550,7 @@ static int encode_execute(struct qaic_device *qdev, struct mem_handle *mem,
 	}
 	/* add post transfer stuff to last segment */
 	i--;
+	mem->reqs[i].cmd |= GEN_COMPLETION;
 	mem->reqs[i].db_addr = db_addr;
 	mem->reqs[i].db_len = db_len;
 	mem->reqs[i].db_data = db_data;
