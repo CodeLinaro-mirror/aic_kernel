f248aa8e4a03fca947b15a341626da882486b8a6
Jeffrey Hugo <quic_jhugo@quicinc.com>
2024-06-12 11:30:29 -0600

    accel/qaic: Add basic AIC200 support
    Add basic support for the new AIC200 product. For now, treat as AIC100
    as AIC200 will have the same basic functionality. The PCIe Device ID for
    the physical function is 0xa110 and the virtual function is 0xa111.
    
    Change-Id: I8a8660af032e14f3f669d0464118654310057e20
    Signed-off-by: Jeffrey Hugo <quic_jhugo@quicinc.com>

--- drivers/accel/qaic/qaic_drv.c
+++ drivers/accel/qaic/qaic_drv.c
@@ -38,6 +38,8 @@
 MODULE_IMPORT_NS(DMA_BUF);
 
 #define PCI_DEV_AIC100			0xa100
+#define PCI_DEV_AIC200PF		0xa110
+#define PCI_DEV_AIC200VF		0xa111
 #define QAIC_NAME			"qaic"
 #define QAIC_DESC			"Qualcomm Cloud AI Accelerators"
 #define CNTL_MAJOR			5
@@ -378,7 +380,8 @@ static struct qaic_device *create_qdev(struct pci_dev *pdev, const struct pci_de
 		return NULL;
 
 	qdev->dev_state = QAIC_OFFLINE;
-	if (id->device == PCI_DEV_AIC100) {
+	if (id->device == PCI_DEV_AIC100 || id->device == PCI_DEV_AIC200PF ||
+	    id->device == PCI_DEV_AIC200VF) {
 		qdev->num_dbc = 16;
 		qdev->dbc = devm_kcalloc(dev, qdev->num_dbc, sizeof(*qdev->dbc), GFP_KERNEL);
 		if (!qdev->dbc)
@@ -640,6 +643,8 @@ static struct mhi_driver qaic_mhi_driver = {
 
 static const struct pci_device_id qaic_ids[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_QCOM, PCI_DEV_AIC100), },
+	{ PCI_DEVICE(PCI_VENDOR_ID_QCOM, PCI_DEV_AIC200PF), },
+	{ PCI_DEVICE(PCI_VENDOR_ID_QCOM, PCI_DEV_AIC200VF), },
 	{ }
 };
 MODULE_DEVICE_TABLE(pci, qaic_ids);
